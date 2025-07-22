a1819527ac0903636093502cb387498aa88c5709
PL <pilouk@gmail.com>
2025-07-22 17:32:06 -0400

    Update index screen

--- client/snap-to-table/app/_layout.tsx
+++ client/snap-to-table/app/_layout.tsx
@@ -1,14 +1,16 @@
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {Stack} from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import "../styles/global.css"
 
+const queryClient = new QueryClient();
 export default function RootLayout() {
-    
-    const queryClient = new QueryClient();
-    
     return (
         <QueryClientProvider client={queryClient}>
-            <Stack/>
+            <StatusBar style="dark"></StatusBar>
+            <Stack>
+                <Stack.Screen name="index" options={{headerShown: false}} />
+            </Stack>
         </QueryClientProvider>
     );
 }
--- client/snap-to-table/app/index.tsx
+++ client/snap-to-table/app/index.tsx
@@ -1,8 +1,45 @@
-import { Text, View } from "react-native";
+// app/index.tsx
+import {Text, View, Pressable, ActivityIndicator, SafeAreaView} from 'react-native';
+import {Camera} from 'lucide-react-native';
+import {usePostRecipeAnalysis} from '../features/recipes/hooks/useRecipe';
+
 export default function Index() {
-  return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-xl">Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    const {error, isPending} = usePostRecipeAnalysis();
+
+    return (
+        <SafeAreaView className="flex-1 bg-zinc-900">
+            <View className="flex-1 items-center justify-center p-8">
+                {/* Main Message */}
+                <Text className="text-4xl font-semibold text-gray-200 text-center mb-10 animate-fadeIn">
+                    Any new recipe for today?
+                </Text>
+
+                {/* Action Button */}
+                <Pressable
+                    className="flex-row items-center justify-center bg-indigo-600 px-8 py-4 rounded-xl shadow-lg
+                     active:bg-indigo-700
+                     disabled:opacity-50
+                     animate-fadeIn-delay-150"
+                >
+                    {isPending ? (
+                        <ActivityIndicator size="small" color="white"/>
+                    ) : (
+                        <>
+                            <Camera color="white" size={24} className="mr-3"/>
+                            <Text className="text-lg font-bold text-white">
+                                Snap a recipe
+                            </Text>
+                        </>
+                    )}
+                </Pressable>
+
+                {/* Error Message */}
+                {error && (
+                    <Text className="text-red-400 mt-6 text-center">
+                        {error.message}
+                    </Text>
+                )}
+            </View>
+        </SafeAreaView>
+    );
 }
